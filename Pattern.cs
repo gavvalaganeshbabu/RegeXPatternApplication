8ca2fde515b8fea20d8875627d9cb9ad57c72388
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-19 09:04:56 +0530

    Check FirstName is Valid or Not

--- Pattern.cs
+++ Pattern.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace RegeXPatternApplication
 {
     internal class Pattern
     {
+        public const string First_NamePattern = "^[A-Z][a-zA-Z]*$";
+        internal void FirstNameValidate(string First_name)
+        {
+            Regex regex = new Regex(First_NamePattern);
+            bool result = regex.IsMatch(First_name);
+            if (result == true)
+                Console.WriteLine("Entered First_name is Valid ");
+            else
+                Console.WriteLine("Entered First_name is Not Valid");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace RegeXPatternApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RegeXpattern Application ");
+            Console.WriteLine("Enter a First Name ");
+            Pattern patternChecker=new Pattern();
+            string First_name=Console.ReadLine();
+            patternChecker.FirstNameValidate(First_name);
+            Console.ReadLine();
         }
     }
 }
