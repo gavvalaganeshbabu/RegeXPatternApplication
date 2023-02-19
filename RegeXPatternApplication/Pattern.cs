4a59df72afadfc416233bf416d8911b3590a0554
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-19 08:40:08 +0530

    Add project files.

---
+++ RegeXPatternApplication/Pattern.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegeXPatternApplication
+{
+    internal class Pattern
+    {
+    }
+}
---
+++ RegeXPatternApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegeXPatternApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RegeXpattern Application ");
+        }
+    }
+}
